342f9c8d08338a589bd390ed17b806461c928819
ficou <ficoupay@gmail.com>
2026-03-10 13:55:02 +0100

    Creando register en auth, continuando con la api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+    $validator = Validator::make($request->all(),[
+            'name' => 'required|string|min:10|max:100',
+            'role' => 'required|string|in:admin,user',
+            'email'=> 'required|string|email|min:10|max:50|unique:users',
+            'password' => 'required|string|min:5|max:30|confirmed',
+
+        ]);
+        if($validator->failed()) {
+            return response()->json(['error' => $validator->errors()], 422);
+         }
+         User::create([
+            'name'=> $request->get('name'),
+            'email' => $request->get(key: 'email'),
+            'role' => $request->get(key: 'role'),
+            'password' => bcrypt($request->get(key: 'password')),
+            ]);
+    }
 }
